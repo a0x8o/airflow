254f6b9d660c29e4ed0cf3f035c697d26615a40e
Alex Taylor-Barreto <alex.barreto@databricks.com>
2022-01-28 16:00:20 -0600

    Clarify default Python rules after dropping 3.6 (#21180)

--- tests/providers/google/cloud/hooks/vertex_ai/__init__.py
+++ tests/providers/google/cloud/hooks/vertex_ai/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,9 +14,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-export PYTHON_MAJOR_MINOR_VERSION="3.7"
-export FORCE_ANSWER_TO_QUESTIONS=${FORCE_ANSWER_TO_QUESTIONS:="quit"}
-export PRINT_INFO_FROM_SCRIPTS="false"
-
-# shellcheck source=scripts/ci/static_checks/flake8.sh
-. "$( dirname "${BASH_SOURCE[0]}" )/../static_checks/flake8.sh" "${@}"
